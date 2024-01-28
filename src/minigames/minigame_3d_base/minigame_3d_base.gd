022a63c5ae6661178fd98c858ecae3001db6a6af
Christopher Ray Langford <chris@hellocld.com>
2024-01-27 19:49:15 -0500

    Add music, busses

--- src/minigames/minigame_3d_base/minigame_3d_base.gd
+++ src/minigames/minigame_3d_base/minigame_3d_base.gd
@@ -2,9 +2,16 @@ class_name MiniGame3DBase
 extends Node3D
 
 @export var game_time:float = 15.0
+
+@export_category("Announcements")
 @export var game_announce_init:String
 @export var game_announce_win:String = "Nice!"
 @export var game_announce_lose:String = "Too Bad!"
+
+@export_category("Music")
+@export var main_song:AudioStream
+@export var win_song:AudioStream
+@export var lose_song:AudioStream
 
 
 func _ready() -> void:
@@ -13,11 +20,17 @@ func _ready() -> void:
 	EventBus.minigame_timer_init.emit(game_time)
 	EventBus.minigame_lost.connect(_on_game_lost)
 	EventBus.minigame_won.connect(_on_game_won)
+	if main_song:
+		MusicPlayer.play_song(main_song)
 
 
 func _on_game_lost() -> void:
 	EventBus.minigame_announce.emit(game_announce_lose)
+	if win_song:
+		MusicPlayer.play_song(lose_song)
 
 
 func _on_game_won() -> void:
 	EventBus.minigame_announce.emit(game_announce_win)
+	if lose_song:
+		MusicPlayer.play_song(win_song)
